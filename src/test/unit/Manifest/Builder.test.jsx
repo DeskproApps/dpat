b40ba83c12035e6aa41abdff1e96e9f8d9427b63
radu <radu.helstern@deskpro.com>
2017-09-15 16:44:22 +0100

    add support validating multiple manifest versions add support for manifest v.2.2.0 make appVersion a required key

--- src/test/unit/Manifest/Builder.test.jsx
+++ src/test/unit/Manifest/Builder.test.jsx
@@ -1,23 +1,33 @@
+const fs = require('fs');
+
 const Builder = require('../../../main/javascript/Manifest/Builder');
 const Manifest = require('../../../main/javascript/Manifest/Manifest');
-
-const manifestJson210 = require.resolve('../../resources/valid-app-manifest-v2.1.0.manifest.json');
-const packageJson210 = require.resolve('../../resources/valid-app-manifest.package-v2.1.0.json');
+const ManifestSchema = require('../../../main/javascript/Manifest/ManifestSchema');
 
 test('can build from src', done =>
 {
-  const manifests = [
-    {
-      name: "manifest.json",
-      path: manifestJson210
-    },
-    {
-      name: "package.json",
-      path: packageJson210
-    },
-  ].map((src) => new Builder().setPropsFromSource(src).build());
+  const manifests = ManifestSchema.getVersions()
+      .map(version => {
+        return {
+          name: 'manifest.json',
+          path: fs.realpathSync(`${__dirname}/../../resources/valid-v${version}-manifest.manifest.json`)
+        };
+      })
+      .map((src) => new Builder().setPropsFromSource(src).build())
+  ;
 
+  const packageJsonManifests = ManifestSchema.getVersions()
+    .map(version => {
+      return {
+        name: 'package.json',
+        path: fs.realpathSync(`${__dirname}/../../resources/valid-v${version}-manifest.package.json`)
+      };
+    })
+    .map((src) => new Builder().setPropsFromSource(src).build())
+  ;
 
   manifests.forEach((manifest) => expect(manifest instanceof Manifest).toEqual(true));
+  packageJsonManifests.forEach((manifest) => expect(manifest instanceof Manifest).toEqual(true));
+
   done();
 });
