a719243a816f5fa4ecd7f8465a4e7dc9766fbace
radu <radu.helstern@deskpro.com>
2018-03-07 17:12:06 +0000

    update travis command

--- src/main/javascript/command/dpat-travis.js
+++ src/main/javascript/command/dpat-travis.js
@@ -4,79 +4,50 @@ const program = require("commander");
 const https = require("https");
 const AsyncAction = require('../../../commander/AsyncAction');
 
-function onAfterSuccess(done)
+/**
+ * @param {{}} travisEnv
+ * @param {{}} opts
+ * @param {function} done
+ */
+function githubPRComment(travisEnv, opts, done)
 {
-  console.log('environment var keys ', Object.keys(process.env));
-
-  const envVars = [
-    'TRAVIS_ALLOW_FAILURE',
-    'TRAVIS_BRANCH',
-    'TRAVIS_BUILD_DIR',
-    'TRAVIS_BUILD_ID',
-    'TRAVIS_COMMIT',
-    'TRAVIS_COMMIT_MESSAGE',
-    'TRAVIS_EVENT_TYPE',
-    'TRAVIS_JOB_ID',
-    'TRAVIS_JOB_NUMBER',
-    'TRAVIS_OS_NAME',
-    'TRAVIS_OSX_IMAGE',
-    'TRAVIS_PULL_REQUEST',
-    'TRAVIS_PULL_REQUEST_BRANCH',
-    'TRAVIS_PULL_REQUEST_SHA',
-    'TRAVIS_PULL_REQUEST_SLUG',
-    'TRAVIS_REPO_SLUG'
-  ];
-
-  for (const v of envVars) {
-    console.log('TRAVIS VAR ', v, process.env[v]);
-  }
-
-  console.log('pull request env var keys ', Object.keys(process.env));
-
-  if ( process.env.TRAVIS_PULL_REQUEST === "false" ) {
+  if ( travisEnv.TRAVIS_PULL_REQUEST === "false" ) {
     console.log('skip adding artifact download url. not a pull request');
     return ;
   }
 
-  if ( !process.env.GITHUB_TOKEN ){
-    console.log('skip adding artifact download url. looks like environment variable GITHUB_TOKEN is not set');
+  if (! travisEnv.ARTIFACTS_BUCKET) {
+    console.log('skip adding artifact download url.is artifact uploading to s3 turned on in .travis.yml ?');
     return ;
   }
 
-  if (! process.env.ARTIFACTS_BUCKET) {
-    console.log('skip adding artifact download url.is artifact uploading to s3 turned on in .travis.yml ?');
-  }
-
   const requestBody = JSON.stringify({
-    body: `TRAVIS-CI: download build from https://s3.amazonaws.com/${process.env.ARTIFACTS_BUCKET}/${process.env.TRAVIS_REPO_SLUG}/${process.env.TRAVIS_BUILD_NUMBER}/dist/app.zip`
+    body: `TRAVIS-CI: download build from https://s3.amazonaws.com/${travisEnv.ARTIFACTS_BUCKET}${opts.s3TargetPath}/dist/app.zip`
   });
 
   const requestOptions = {
     hostname: "api.github.com",
     method: "POST",
-    path: `/repos/${process.env.TRAVIS_REPO_SLUG}/issues/${process.env.TRAVIS_PULL_REQUEST}/comments`,
+    path: `/repos/${travisEnv.TRAVIS_REPO_SLUG}/issues/${travisEnv.TRAVIS_PULL_REQUEST}/comments`,
     headers: {
       'Content-Length': Buffer.byteLength(requestBody),
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `token ${process.env.GITHUB_TOKEN}`,
-      'User-Agent': '@deskpro/apps-youtrack'
+      'Authorization': `token ${travisEnv.GITHUB_TOKEN}`,
+      'User-Agent': travisEnv.TRAVIS_REPO_SLUG
     }
   };
 
   const request = https.request(requestOptions, (res) => {
     res.setEncoding('utf8');
 
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
-
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-
-    res.on('end', () => {
-      console.log('No more data in response.');
-      done();
-    });
+    if (opts.debug) {
+      console.log('statusCode:', res.statusCode);
+      console.log('headers:', res.headers);
+      res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+      });
+    }
+    res.on('end', () => done());
   });
 
   request.on('error', (e) => {
@@ -88,25 +59,36 @@ function onAfterSuccess(done)
 }
 
 /**
+ * @param {String} subcommand
  * @param {String} path
  * @param {Command} cmd
  * @param {function} done
  */
-function action(path, cmd, done)
+function action(subcommand, path, cmd, done)
 {
-  if (! cmd.step) {
-    console.log('ERROR: missing required argument: --step');
-    process.exit(1);
+  if (subcommand === 'pr-comment') {
+    if (! cmd.s3target) {
+      console.error('ERROR: missing required argument: --s3-target-path');
+      process.exit(1);
+
+      if (process.env.GITHUB_TOKEN) {
+        githubPRComment(process.env, {
+          debug: !!cmd.debug,
+          s3TargetPath: cmd.s3target
+        }, done);
+        return ;
+      }
+      console.warn('skip adding comment on pull request. no vcs configuration detected')
+    }
   }
 
-  if (cmd.step === 'after_success') {
-    onAfterSuccess(done)
-  }
+  console.warn('skip adding comment on pull request. no vcs configuration detected')
 }
 
 program
   .version("0.1.0", "-V, --version")
-  .arguments("<path>", "r")
-  .option("-s, --step <step>", "the travis step to invoke inside the project at <path>")
+  .arguments("<subcommand> <path> ", "r")
+  .option("-t, --s3-target-path <s3target>", "the s3 path under which the build artifacts will be uploaded")
+  .option("-d, --debug [debug]", "turn on debugging")
   .action(AsyncAction(action))
   .parse(process.argv);
