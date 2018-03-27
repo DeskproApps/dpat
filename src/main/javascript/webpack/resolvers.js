0d6e7b5750c5c19d47ff6a95e54dcbb213d94b03
radu <radu.helstern@deskpro.com>
2018-03-27 11:02:23 +0100

    add jsdoc comments

--- src/main/javascript/webpack/resolvers.js
+++ src/main/javascript/webpack/resolvers.js
@@ -1,6 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 
+/**
+ * Resolves a relative path in the context of a module into an absolute path
+ *
+ * @param {string} moduleName
+ * @param {string} relativePath the relative path inside the module
+ * @returns {string|null}
+ */
 function resolvePath (moduleName, relativePath)
 {
   if (! moduleName) {
@@ -37,16 +44,29 @@ function resolvePath (moduleName, relativePath)
   return null;
 }
 
-
 module.exports = {
+
+  /**
+   * Resolves the path to the webpack-dev-server.js executable
+   *
+   * @param {string} [moduleName] the webpack dev server package name, defaults to webpack-dev-server
+   * @returns {string|null}
+   */
   resolveBinWebpackDevServer: function (moduleName) {
     "use strict";
     const relativePath = path.join('bin', 'webpack-dev-server.js');
-    return resolvePath(moduleName, relativePath);
+    return resolvePath(moduleName || 'webpack-dev-server' , relativePath);
   },
+
+  /**
+   * Resolves the path to the webpack.js executable
+   *
+   * @param {string} [moduleName] the webpack package name, defaults to webpack
+   * @returns {string|null}
+   */
   resolveBinWebpack: function (moduleName) {
     "use strict";
     const relativePath = path.join('bin', 'webpack.js');
-    return resolvePath(moduleName, relativePath);
-  },
+    return resolvePath(moduleName || 'webpack', relativePath);
+  }
 };
